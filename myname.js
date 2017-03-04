05f12d79510e2679f4ed70f30690c3f5ac8b1d21
aidana12 <ai.mun1469@gmail.com>
2017-03-04 19:22:33 +0100

    Update1

--- myname.js
+++ myname.js
@@ -1,26 +1,20 @@
-$('body').scrollspy({ target: '#navbar-example' });
-$('[data-spy="scroll"]').each(function () {
-  var $spy = $(this).scrollspy('refresh')
-});
-// http://www.ordinarycoder.com/jquery-fade-content-scroll/
+$(document).ready(function() {
+    $('body').scrollspy({ target: '#navbar-example' });
+    $('[data-spy="scroll"]').each(function () {
+      var $spy = $(this).scrollspy('refresh')
+  });
 $(function() {
-    $(window).scroll(function(){
-    
-       
-        $('.fadeInBlock').each( function(i){
-            
+    $(window).scroll(function(){      
+        $('.fadeInBlock').each(function(i){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
              // Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  
-            bottom_of_window = bottom_of_window;  
-          
-            if( bottom_of_window > bottom_of_object ){
-                
+             bottom_of_window = bottom_of_window;  
+             if( bottom_of_window > bottom_of_object ){
                 $(this).animate({'opacity':'1'},2000);
-                    
             }
         }); 
-    
+
     });
+});
 });
